5da5bd3cbbdfde96d8cb939146265c011760436e
Jai Kumar Gaja <gajajaikumar1@gmail.com>
2026-09-21 17:23:32 +0530

    First Commit

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+variable "mongo_uri" {
+  description = "MongoDB Connection String"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# 1. Open the necessary ports
+resource "aws_security_group" "web_sg" {
+  name        = "flask_express_jenkins_sg"
+  description = "Allow SSH, Express (4000), and Flask (5000)"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 4000  
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 2. Find the latest Ubuntu image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+# 3. Create the empty server
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  
+  # Tell AWS to inject your public key into the server
+  key_name = "aws-assignment-key"
+
+  user_data = templatefile("init_script.tftpl", {
+  mongo_uri_secret = var.mongo_uri
+})
+
+  tags = {
+    Name = "flask_express_jenkins-Single-EC2"
+  }
+}
+
+# 4. Output the IP address to your terminal
+output "public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
